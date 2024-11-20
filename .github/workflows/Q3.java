a95c84e66e22ff4952125851094d0f757d62254c
Louis Marouard <128597536+Vermoud27@users.noreply.github.com>
2024-11-20 13:53:14 +0100

    Update Q3.java

--- .github/workflows/Q3.java
+++ .github/workflows/Q3.java
@@ -1,9 +1,1 @@
-public class Q3 {
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        for (int i = 0; i < n; i++) {
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-}
+
