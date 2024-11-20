e970ffe06bd72d4e2065012e3f5b54adcf98926f
Louis Marouard <128597536+Vermoud27@users.noreply.github.com>
2024-11-20 13:46:02 +0000

    modif Q3

--- Q3.java
+++ Q3.java
@@ -2,7 +2,7 @@ public class Q3 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         for (int i = 0; i < n; i++) {
-            System.out.print("*");
+            System.out.print("**");
         }
         System.out.println();
     }
