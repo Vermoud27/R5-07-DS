537d19076c8657e76d737a377c0f02506882705b
Louis Marouard <128597536+Vermoud27@users.noreply.github.com>
2024-11-20 14:31:14 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,11 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
